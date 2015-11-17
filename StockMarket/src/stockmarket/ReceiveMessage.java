9ef7dc0afb83d58d14a2120dbb707ec320d46615
Tom Newton <tanewton95@gmail.com>
2015-11-17 15:18:45 +0000

    Removed Bullshit

--- StockMarket/src/stockmarket/ReceiveMessage.java
+++ StockMarket/src/stockmarket/ReceiveMessage.java
@@ -1,17 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package stockmarket;
 
 
 import java.net.*;
 import java.io.*;
-/**
- *
- * @author Computing
- */
+
 public class ReceiveMessage extends Thread {
     private Socket socket;
     private BufferedReader in = null;
