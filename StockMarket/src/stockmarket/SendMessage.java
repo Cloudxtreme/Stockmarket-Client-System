32bc551862e48b5b675eb7687a5918a2d7f06dee
Tom Newton <tanewton95@gmail.com>
2015-11-17 15:59:59 +0000

    Removed Thead
    Thread unnecessary

--- StockMarket/src/stockmarket/SendMessage.java
+++ StockMarket/src/stockmarket/SendMessage.java
@@ -1,10 +1,11 @@
+
 package stockmarket;
 
 import java.net.*;
 import java.io.*;
 
 
-public class SendMessage extends Thread {   
+public class SendMessage {   
     
     private Socket socket;
     private PrintWriter out = null;
@@ -23,12 +24,5 @@ public class SendMessage extends Thread {
     public void sendMessage(String message)
     {
         out.println(message);
-    }
-    
-    @Override
-    public void run(){
-        String line = null;
-        //Set line to a variable that is needed to be sent
-        sendMessage(line);
     }
 }
